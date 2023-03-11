ce43031920e4c37483e6d66e73bce01d30e06804
Brightini <brightiniabasi@gmail.com>
2023-03-11 03:53:58 +0100

    Add string format :ok_hand:

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -6,4 +6,4 @@ def uppercase(str):
         if ord(char) >= 97 and ord(char) <= 122:
             char = chr(ord(char) - 32)
         result += char
-    print(result)
+    print("{}".format(result))
