d093e8c7d5dd80fff2a65e24104dd936bc8152b5
Brightini <brightiniabasi@gmail.com>
2023-03-13 00:04:56 +0100

    Update code :ok_hand:

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,9 +3,11 @@ from sys import argv
 if __name__ == '__main__':
     # -1 to exclude the name of script
     count = len(argv)
-    if count - 1 == 0:
-        print("{} argument.".format(count - 1))
+    if count == 1:
+        print("{} arguments.".format(count - 1))
+    elif count == 2:
+        print("{} argument:".format(count - 1))
     else:
         print("{} arguments:".format(count - 1))
-        for i in range(1, count):
-            print("{}: {}".format(i, argv[i]))
+    for i in range(1, count):
+        print("{}: {}".format(i, argv[i]))
