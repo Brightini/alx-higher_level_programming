e92a1aae4d827c3d12dbdcff990d1ef3fa3437a1
Brightini <brightiniabasi@gmail.com>
2023-03-12 23:58:30 +0100

    Initial commit :tada:

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+from sys import argv
+if __name__ == '__main__':
+    # -1 to exclude the name of script
+    count = len(argv)
+    if count - 1 == 0:
+        print("{} argument.".format(count - 1))
+    else:
+        print("{} arguments:".format(count - 1))
+        for i in range(1, count):
+            print("{}: {}".format(i, argv[i]))
