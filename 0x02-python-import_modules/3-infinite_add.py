dc30cf2b68fdbf047143dd2c459684b06641af25
Brightini <brightiniabasi@gmail.com>
2023-03-13 02:29:25 +0100

    Add: gaurd clause :ok_hand:

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-from sys import argv
-sum = 0
-count = len(argv)
-for i in range(1, count):
-    sum += int(argv[i])
-print("{}".format(sum))
+if __name__ == '__main__':
+    from sys import argv
+    sum = 0
+    count = len(argv)
+    for i in range(1, count):
+        sum += int(argv[i])
+    print("{}".format(sum))
