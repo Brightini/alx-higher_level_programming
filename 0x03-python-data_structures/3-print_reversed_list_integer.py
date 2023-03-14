3c694bf762fda2696e9945f87a50c1d294fd30fb
Brightini <brightiniabasi@gmail.com>
2023-03-14 16:50:56 +0100

    Fix: variable type bug :bug:

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -6,9 +6,10 @@ def print_reversed_list_integer(my_list=[]):
     Args:
         my_list: list of integers
     """
-    idx = len(my_list)
-    if my_list is None or idx == 0:
+
+    if my_list is None:
         return
+    idx = len(my_list)
     for item in range(idx - 1, -1, -1):
         if not isinstance(my_list[item], int):
             continue
