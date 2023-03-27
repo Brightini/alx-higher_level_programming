b971f24926e6328865cdbaa434c90361c3abeb6b
Brightini <brightiniabasi@gmail.com>
2023-03-27 19:23:06 +0100

    Update except block

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -8,6 +8,6 @@ def safe_print_list(my_list=[], x=0):
             print("{:d}".format(i), end="")
             count += 1
         except Exception as e:
-            print("Theres a problem: {}".format(type(e)))
+            pass
     print()
     return count
