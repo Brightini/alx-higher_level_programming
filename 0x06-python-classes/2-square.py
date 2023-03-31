72ecc68936df04d875a0e257db16f2b6c852488d
Brightini <brightiniabasi@gmail.com>
2023-03-31 18:53:31 +0100

    Add docstring :books:

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
+"""Square module"""
+
+
 class Square():
+    """A Square class"""
     def __init__(self, size=0):
+        """Iniatializes an instance attribute(s)"""
         if type(size) != int:
             raise TypeError("size must be an integer")
         elif size < 0:
--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
+"""Square module"""
+
+
 class Square():
+    """A Square class"""
     def __init__(self, size=0):
+        """Iniatializes an instance attribute(s)"""
         if type(size) != int:
             raise TypeError("size must be an integer")
         elif size < 0:
@@ -8,4 +13,5 @@ class Square():
         self.__size = size
 
     def area(self):
+        """Computes and returns the area of a Square object"""
         return self.__size ** 2
