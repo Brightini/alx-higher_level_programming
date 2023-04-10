3672f74caf05db05ed34d68b888be7eb10aaf3a1
Brightini <brightiniabasi@gmail.com>
2023-04-10 23:54:09 +0100

    Handled exception for empty arguments

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -2,7 +2,7 @@
 """3-say_my_name module"""
 
 
-def say_my_name(first_name, last_name=""):
+def say_my_name(first_name="", last_name=""):
     """
     prints first and last name
 
@@ -13,7 +13,7 @@ def say_my_name(first_name, last_name=""):
     Raises:
         TypeError: if either @first_name or @last_name is not a string
     """
-    if not isinstance(first_name, str):
+    if not isinstance(first_name, str) or not first_name:
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
