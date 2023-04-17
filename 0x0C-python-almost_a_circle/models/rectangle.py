d5e155fc23a7075d2b07222f27e7dd8591b8627c
Brightini <brightiniabasi@gmail.com>
2023-04-17 23:45:52 +0100

    Add rectangle module :tada:

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python3
+"""Defines Rectangle class"""
+from models.base import Base
+
+
+class Rectangle(Base):
+    """A Rectangle class"""
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """Instantiates Rectangle instances"""
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+        super().__init__(id)
+
+    @property
+    def width(self):
+        """Gets width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Sets width"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Gets height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Sets height"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
+        self.__height = value
+
+    @property
+    def x(self):
+        """Gets x"""
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """Sets x"""
+        if not isinstance(value, int):
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
+        self.__x = value
+
+    @property
+    def y(self):
+        """Gets y"""
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """Sets y"""
+        if not isinstance(value, int):
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
+        self.__y = value
