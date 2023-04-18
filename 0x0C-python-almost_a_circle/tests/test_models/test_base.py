71f7ef0531b6b0fafac66cf5de16541ede67a052
Brightini <brightiniabasi@gmail.com>
2023-04-18 14:27:34 +0100

    Updated rectangle.py :zap: ; Added its test module

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,11 +1,19 @@
+"""Defines test cases for Base class"""
 import unittest
 from models.base import Base
 
+
 class TestBaseClass(unittest.TestCase):
-    def test_id(self):
-        """test instance id"""
-        b1 = Base()
-        self.assertEqual(b1.id, 1)
-        b2 = Base(12)
-        self.assertEqual(b2.id, 12)
-        
+    """Test cases for Base class"""
+    def test_id_passed(self):
+        b1 = Base(12)
+        self.assertEqual(b1.id, 12)
+
+    def test_id_not_passed(self):
+        b2 = Base()
+        self.assertEqual(b2.id, 1)
+
+    def test_private_class_instance(self):
+        b3 = Base()
+        with self.assertRaises(AttributeError):
+            print(b3.__nd_objects)
---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -0,0 +1,58 @@
+"""Defines test cases for Rectangle class"""
+import unittest
+from models.rectangle import Rectangle
+
+
+class TestRectangleClass(unittest.TestCase):
+    """Test cases for rectangle class"""
+
+    @classmethod
+    def setUpClass(cls):
+        cls.r1 = Rectangle(1, 2)
+        cls.r2 = Rectangle(1, 2, 3)
+        cls.r3 = Rectangle(1, 2, 3, 4)
+        cls.r4 = Rectangle(1, 2, 3, 4, 15)
+
+    def test_id_passed(self):
+        self.assertEqual(self.r1.id, 3)
+        self.assertEqual(self.r2.id, 4)
+        self.assertEqual(self.r3.id, 5)
+        self.assertEqual(self.r4.id, 15)
+
+    def test_width_setter(self):
+        """Test width setter"""
+        self.r4.width = 10
+        self.assertEqual(self.r4.width, 10)
+
+    def test_height_getter(self):
+        """Test height getter"""
+        self.r4.height = 7
+        self.assertEqual(self.r4.height, 7)
+
+    def test_x_getter(self):
+        """Test x getter"""
+        self.r4.x = 9
+        self.assertEqual(self.r4.x, 9)
+
+    def test_y_getter(self):
+        """Test y getter"""
+        self.r4.y = 7
+        self.assertEqual(self.r4.y, 7)
+
+    def test_attribute_error(self):
+        """Test for errors in instance attributes"""
+        with self.assertRaises(TypeError):
+            Rectangle(1, "h")
+            Rectangle(6.98, 4)
+        with self.assertRaises(ValueError):
+            Rectangle(0, 6)
+            Rectangle(-1, 7)
+            Rectangle(5, -3)
+            Rectangle(8, 0)
+            Rectangle(2, 1, -7, 9)
+            Rectangle(5, 9, 2, -10)
+
+    def test_area(self):
+        """Test for area"""
+        self.assertEqual(self.r1.area(), 2)
+        self.assertEqual(self.r4.area(), 2)
