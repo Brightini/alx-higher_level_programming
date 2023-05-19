891c056d533cd7177b67f189bd88236e9331027b
brightini <brightiniabasi@gmail.com>
2023-05-19 11:41:28 -0700

    Grant user all privileges

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -5,3 +5,7 @@ CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 
 -- Reload grant tables to ensure new privileges are put into effect
 FLUSH PRIVILEGES;
+
+-- Grant all privileges
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost'
+FLUSH PRIVILEGES;
