296d79d42cbfe2656d390d5bc2d5bd39c9358a84
brightini <brightiniabasi@gmail.com>
2023-05-19 11:37:39 -0700

    Added flush privileges

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,2 +1,7 @@
 -- This script creates a MySQL server user
+
+-- Create user
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+
+-- Reload grant tables to ensure new privileges are put into effect
+FLUSH PRIVILEGES;
