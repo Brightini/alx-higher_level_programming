bd5996d0abf8489fa63db70d44e03d8428422883
brightini <brightiniabasi@gmail.com>
2023-05-20 03:48:46 -0700

    Initial commit :tada:

---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,9 @@
+-- This script lists all records in a table that can be found in another table
+
+SELECT * FROM cities
+WHERE state_id = (
+	SELECT id
+	FROM states
+	WHERE name = 'California'
+)
+ORDER BY id;
