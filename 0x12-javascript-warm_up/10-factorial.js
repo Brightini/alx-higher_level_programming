b1b06497eadeeb39e9df3062f02d1c3e171ac287
Brightini <brightiniabasi@gmail.com>
2023-06-14 19:48:33 +0100

    Initial commit :tada:

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+let result;
+result = factorial(Number(process.argv[2]));
+console.log(result);
+
+function factorial (num) {
+  let value = 1;
+  if (isNaN(num) || num === 1) {
+    return (1);
+  } else {
+    value = num * factorial(num - 1);
+    return (value);
+  }
+}
