948637fee5833e1478116c062410a65696c5b8fc
Brightini <brightiniabasi@gmail.com>
2023-06-16 20:58:35 +0100

    Initial commit :tada:

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,16 +1,15 @@
-const Square = require('./5-square');
+const OldSquare = require('./5-square');
 
-module.exports = class Square extends Square {
+module.exports = class Square extends OldSquare {
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
 
-  charPrint(c) {
-	if (c !== undefined) {
-		for (let i = 0; i < this.height; i++) {
-			let row = '';
-			for (let j = 0; j < this.width; j++) {
-			  row += c;
-			}
-			console.log(row);
-		  }
-	}
+    for (let i = 0; i < this.height; i++) {
+      let square = '';
+      square += c.repeat(this.width);
+      console.log(square);
+    }
   }
 };
