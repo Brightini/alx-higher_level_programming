361ef8b6d448f1a7711d00c6ce04ca6302de3aee
Brightini <brightiniabasi@gmail.com>
2023-07-04 13:48:17 -0700

    Initial commit

---
+++ 0x10-python-network_0/0-body_size.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/bash
+# this script that takes in a URL, sends a request to that URL,
+# and displays the size of the body of the response
+
+response=$(curl -s -o /dev/null/ -w "%{size_download}" "$1")
+echo $response
