5bf0891812b29ac73c2f93c52c6d9db77b0812ad
Brightini <brightinibasi@gmail.com>
2023-07-05 13:13:16 -0700

    Updated code

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,16 +1,3 @@
 #!/bin/bash
 # This script takes a URL, sends a GET request and display the body of a 200 status code response
-
-URL=$1
-response=$(curl -s -w "%{http_code}" "$URL")
-
-# Extract the last 3 characters as the status code
-status_code=${response: -3}
-# Extract the response body, removing the status code
-body=${response%"$status_code"}
-
-if [[ $status_code == "200" ]]; then
-	echo "$body"
-else
-	echo "Error: Unexpected status code $status_code"
-fi
+curl -s -w "\nResponse body:\n%{http_code}\n" "$1" | awk '/^Response body:$/{p=1;next}p'
