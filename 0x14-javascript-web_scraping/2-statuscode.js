ff1b7985310e7254e914385adc68afe8f105ece5
Brightini <brightiniabasi@gmail.com>
2023-07-29 09:22:12 +0100

    Fixed request module bug

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,10 +1,16 @@
 #!/usr/bin/node
 // This script displays the status code of a GET request
 
-const url = process.argv[3];
 const request = require('request');
 
-request.get(url, (error, response) => {
-  if (error) throw error;
-  console.log('code: ', response.statusCode);
+const options = {
+  url: process.argv[2],
+  method: 'GET'
+};
+
+request(options, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  }
+  console.log('code:', response.statusCode);
 });
